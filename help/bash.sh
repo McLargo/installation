697b3afd5b70b84b46bc46091c17065b4f29ffd6
Javier Gil <javiergilmont@gmail.com>
2025-02-10 14:00:42 +0100

    fix: scape double quotes in help bash file

--- help/bash.sh
+++ help/bash.sh
@@ -12,17 +12,17 @@ echo -e "\t${BLUE}ag - search ignoring extension or pattern${NC}\t${YELLOW}ag co
 echo -e
 echo -e "\t${BLUE}aws - sync to aws bucket${NC}\t\t\t${YELLOW}aws s3 sync /local_path s3://bucket_name/ --dryrun --exclude=\"filesOrFolders\"${NC}"
 echo -e
-echo -e "\t${BLUE}bash - check number of parameters${NC}\t\t${YELLOW}[ "$#" -ne 2 ] || { echo "[ERROR] Illegal number of parameters. Aborting..."; exit 1; }${NC}"
-echo -e "\t${BLUE}bash - check variable is integer${NC}\t\t${YELLOW}[[ "$number" =~ ^[0-9]+$ ]] || { echo "[ERROR] Version is not a number. Aborting..."; exit 1; } ; then${NC}"
+echo -e "\t${BLUE}bash - check number of parameters${NC}\t\t${YELLOW}[ \"$#\" -ne 2 ] || { echo \"[ERROR] Illegal number of parameters. Aborting...\"; exit 1; }${NC}"
+echo -e "\t${BLUE}bash - check variable is integer${NC}\t\t${YELLOW}[[ \"$number\" =~ ^[0-9]+$ ]] || { echo \"[ERROR] Version is not a number. Aborting...\"; exit 1; }${NC}"
 echo -e "\t${BLUE}bash - load file if exists${NC}\t\t\t${YELLOW}[ -f ~/.bash_aliases ] && . ~/.bash_aliases${NC}"
 echo -e
 echo -e "\t${BLUE}cat -redirect output to file${NC}\t\t\t${YELLOW}cat scripts/dump/*.sql  > scripts/db/name.sql${NC}"
 echo -e
-echo -e "\t${BLUE}find - find by type extension${NC}\t\t\t${YELLOW}find . -name "*.py" -type f${NC}"
-echo -e "\t${BLUE}find - delete type extension${NC}\t\t\t${YELLOW}find . -name "*.pyc" -type f -delete${NC}"
+echo -e "\t${BLUE}find - find by type extension${NC}\t\t\t${YELLOW}find . -name \"*.py\" -type f${NC}"
+echo -e "\t${BLUE}find - delete type extension${NC}\t\t\t${YELLOW}find . -name \"*.pyc\" -type f -delete${NC}"
 echo -e "\t${BLUE}find - find and delete by name${NC}\t\t\t${YELLOW}find . ! -name u ! -name p -maxdepth 1 -type f -delete${NC}"
 echo -e
-echo -e "\t${BLUE}grep - search string recursively in python file${NC}\t${YELLOW}grep -rl "string" . --include "*.py"${NC}"
+echo -e "\t${BLUE}grep - search string recursively in python file${NC}\t${YELLOW}grep -rl \"string\" . --include \"*.py\"${NC}"
 echo -e
 echo -e "\t${BLUE}mysqldump - dump a mysql database${NC}\t\t${YELLOW}mysqldump {database} -u{user} -p{password} > ~/.mysqldumps/{filename}.sql${NC}"
 echo -e
